08c2c4ff894db456fb171c5bfd63764ce8cdc676
Eric Frees <efrees@atlasrfid.com>
2017-12-02 23:22:41 -0600

    Day 3, part 1 solved.

---
+++ AdventOfCode2017/Md5Hasher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode2016.Solvers
+{
+    internal class Md5Hasher
+    {
+        private readonly MD5 _hashFunction = MD5.Create();
+
+        public virtual string HashDataAsHexString(string testData)
+        {
+            _hashFunction.Initialize();
+            var hashBytes = _hashFunction.ComputeHash(Encoding.ASCII.GetBytes(testData));
+
+            return BitConverter.ToString(hashBytes, 0, hashBytes.Length)
+                .Replace("-", string.Empty);
+        }
+    }
+}
---
+++ AdventOfCode2017/Solvers/Day3Solver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AdventOfCode2016;
+
+namespace AdventOfCode2017.Solvers
+{
+    internal class Day3Solver
+    {
+        private const int PuzzleInput = 265149;
+
+        public static Day3Solver Create()
+        {
+            return new Day3Solver();
+        }
+
+        public void Solve(string fileText)
+        {
+            var sideLength = 1;
+            var innerTotal = 1;
+
+            while (innerTotal < PuzzleInput)
+            {
+                sideLength += 2;
+                innerTotal += 4 * (sideLength - 1);
+                //Console.WriteLine(innerTotal);
+            }
+
+            //back up a step
+            innerTotal -= 4 * (sideLength - 1);
+
+            //next is above bottom right
+            var relativeX = sideLength / 2;
+            var relativeY = -sideLength / 2 + 1;
+            innerTotal++;
+
+            if (innerTotal < PuzzleInput)
+            {
+                var change = Math.Min(PuzzleInput - innerTotal, sideLength - 2);
+                innerTotal += change;
+                relativeY += change;
+            }
+
+            if (innerTotal < PuzzleInput)
+            {
+                var change = Math.Min(PuzzleInput - innerTotal, sideLength - 1);
+                innerTotal += change;
+                relativeX -= change;
+            }
+
+            if (innerTotal < PuzzleInput)
+            {
+                var change = Math.Min(PuzzleInput - innerTotal, sideLength - 1);
+                innerTotal += change;
+                relativeY -= change;
+            }
+
+            if (innerTotal < PuzzleInput)
+            {
+                var change = Math.Min(PuzzleInput - innerTotal, sideLength - 1);
+                innerTotal += change;
+                relativeX += change;
+            }
+
+            var answer = Math.Abs(relativeX) + Math.Abs(relativeY);
+            Console.WriteLine($"P1: {answer}");
+        }
+    }
+}
