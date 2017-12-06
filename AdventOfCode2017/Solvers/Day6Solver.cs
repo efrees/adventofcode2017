f2653cc707d5c4a4f150490436da4ff20e94c95e
Eric Frees <efrees@atlasrfid.com>
2017-12-05 23:38:24 -0600

    Solve Day 6. Unnecessary code removed after submission.

---
+++ AdventOfCode2017/Solvers/Day6Solver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Solvers
+{
+    internal class Day6Solver : IProblemSolver
+    {
+        public static IProblemSolver Create()
+        {
+            return new Day6Solver();
+        }
+
+        public void Solve(string fileText)
+        {
+            SolvePart1(fileText);
+        }
+
+        private Dictionary<string, int> _statesSeen = new Dictionary<string, int>();
+        private void SolvePart1(string fileText)
+        {
+            var strings = fileText.Split('\t')
+                .ToArray();
+            var currentState = Array.ConvertAll(strings, int.Parse); ;
+            var count = 0;
+
+            while (StateNotSeenBefore(currentState))
+            {
+                LogAsSeen(currentState, count);
+                Redistribute(currentState);
+
+                count++;
+            }
+
+            var targetState = string.Join(",", currentState);
+            var loopLength = count - _statesSeen[targetState];
+
+            Console.WriteLine($"P1: {count}");
+            Console.WriteLine($"P2: {loopLength}");
+        }
+
+        private void LogAsSeen(int[] previous, int index)
+        {
+            var newArray = string.Join(",", previous);
+            _statesSeen.Add(newArray, index);
+        }
+
+        private bool StateNotSeenBefore(int[] nextState)
+        {
+            var nextStateString = string.Join(",", nextState);
+            return !_statesSeen.ContainsKey(nextStateString);
+        }
+
+        private static void Redistribute(int[] nextState)
+        {
+            var max = nextState.Max();
+            var index = nextState.ToList().FindIndex(x => x == max);
+
+            nextState[index] = 0;
+            for (var i = 1; max > 0; i++, max--)
+            {
+                nextState[(index + i) % nextState.Length]++;
+            }
+        }
+    }
+}
