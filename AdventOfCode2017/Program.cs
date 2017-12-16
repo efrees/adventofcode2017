0bc3d33b2826e3efeca22f863b42b78db7293465
Eric Frees <efrees@atlasrfid.com>
2017-12-15 23:27:22 -0600

    Solve Day 16. Original, outputting only part 2.

--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using AdventOfCode2017.Solvers;
 
 namespace AdventOfCode2017
@@ -12,8 +13,8 @@ namespace AdventOfCode2017
         [STAThread]
         static void Main(string[] args)
         {
-            var fileText = GetInputFromFile("day15input.txt");
-            TimeAction(() => Day15Solver.Create().Solve(fileText));
+            var fileText = GetInputFromFile("day16input.txt");
+            Day16Solver.Create().Solve(fileText);
 
             Console.ReadKey();
         }
@@ -38,6 +39,70 @@ namespace AdventOfCode2017
 Hi: {times.Max():N3}ms
 Lo: {times.Min():N3}ms
 Av: {times.Average():N3}ms");
+        }
+    }
+
+    internal class Day16Solver : IProblemSolver
+    {
+        public static IProblemSolver Create()
+        {
+            return new Day16Solver();
+        }
+
+        public void Solve(string fileText)
+        {
+            var moves = fileText.Split(',');
+            var line = Enumerable.Range(0, 16).Select(i => (char)(i + 'a')).ToArray();
+            var seenStates = new Dictionary<string, int>();
+            var step = 0;
+            var remainingSteps = 1000000000;//billion
+            var currentStart = 0;
+            while (remainingSteps > 0)
+            {
+                if (!seenStates.ContainsKey(new string(line)))
+                {
+                    seenStates[new string(line)] = step;
+                }
+                else
+                {
+                    //loop found
+                    var loopSize = step - seenStates[new string(line)];
+                    remainingSteps = remainingSteps % loopSize;
+                }
+                foreach (var move in moves)
+                {
+                    switch (move[0])
+                    {
+                        case 's':
+                            var spin = int.Parse(move.Substring(1));
+                            currentStart -= spin;
+                            currentStart = InRange(currentStart);
+                            break;
+                        case 'p':
+                            var ai = Array.IndexOf(line, move[1]);
+                            var bi = Array.IndexOf(line, move[3]);
+                            line[ai] = move[3];
+                            line[bi] = move[1];
+                            break;
+                        case 'x':
+                            var indexes = move.Substring(1).Split('/').Select(int.Parse).ToArray();
+                            var t = line[InRange(currentStart + indexes[0])];
+                            line[InRange(currentStart + indexes[0])] = line[InRange(currentStart + indexes[1])];
+                            line[InRange(currentStart + indexes[1])] = t;
+                            break;
+                    }
+                }
+                step++;
+                remainingSteps--;
+            }
+            var answer = string.Join("", line.Concat(line).Skip(InRange(currentStart)).Take(16));
+            Output.Answer(answer);
+        }
+
+        private int InRange(int x)
+        {
+            while (x < 0) x += 16;
+            return x % 16;
         }
     }
 }
