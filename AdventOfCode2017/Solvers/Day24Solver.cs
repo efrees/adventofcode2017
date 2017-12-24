7d416f5ae53f42b89485cbe44d0faf391306c3d7
Eric Frees <efrees@atlasrfid.com>
2017-12-23 23:40:11 -0600

    Initial clean up of Day 24. Update stats.

--- AdventOfCode2017/Solvers/Day24Solver.cs
+++ AdventOfCode2017/Solvers/Day24Solver.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace AdventOfCode2017.Solvers
@@ -17,15 +16,29 @@ namespace AdventOfCode2017.Solvers
 
         private void SolvePart1(string fileText)
         {
-            var components = fileText.SplitIntoLines()
-                .Select(ParseComponent)
-                .SelectMany(x => new[] { new { End = x.Ends[0], Component = x }, new { End = x.Ends[1], Component = x } })
-                .ToLookup(x => x.End)
-                .ToDictionary(x => x.Key, x => x.Select(y => y.Component).ToList());
-
-
+            var components = ParseComponentsFromInput(fileText);
             var answer = FindHighestScore(components);
             Output.Answer(answer);
+        }
+
+        private void SolvePart2(string fileText)
+        {
+            var components = ParseComponentsFromInput(fileText);
+            var answer = FindLongestBridge(components);
+            Output.Answer(answer);
+        }
+
+        private Dictionary<int, List<BridgeComponent>> ParseComponentsFromInput(string fileText)
+        {
+            return fileText.SplitIntoLines()
+                .Select(ParseComponent)
+                .SelectMany(x => new[]
+                {
+                    new { Port = x.Ports[0], Component = x },
+                    new { Port = x.Ports[1], Component = x }
+                })
+                .ToLookup(x => x.Port)
+                .ToDictionary(x => x.Key, x => x.Select(y => y.Component).ToList());
         }
 
         private int FindHighestScore(Dictionary<int, List<BridgeComponent>> components)
@@ -34,21 +47,17 @@ namespace AdventOfCode2017.Solvers
             return FindHighestScoreRecursive(0, components, usedSet);
         }
 
-        private int FindHighestScoreRecursive(int currentSize, Dictionary<int, List<BridgeComponent>> components, HashSet<BridgeComponent> usedSet)
+        private int FindHighestScoreRecursive(int portTypeToConnect, Dictionary<int, List<BridgeComponent>> components, HashSet<BridgeComponent> usedSet)
         {
-            if (!components.ContainsKey(currentSize))
-                return 0;
-
-            var options = components[currentSize];
             var maxScore = 0;
-            foreach (var comp in options)
+            foreach (var comp in components[portTypeToConnect].Except(usedSet))
             {
                 if (usedSet.Contains(comp))
                     continue;
 
                 usedSet.Add(comp);
-                var otherEnd = comp.Ends[0] == currentSize ? comp.Ends[1] : comp.Ends[0];
-                var thisComponentScore = comp.Ends.Sum();
+                var otherEnd = comp.Ports[0] == portTypeToConnect ? comp.Ports[1] : comp.Ports[0];
+                var thisComponentScore = comp.Ports.Sum();
                 var score = thisComponentScore + FindHighestScoreRecursive(otherEnd, components, usedSet);
 
                 if (score > maxScore)
@@ -62,39 +71,20 @@ namespace AdventOfCode2017.Solvers
             return maxScore;
         }
 
-        private void SolvePart2(string fileText)
-        {
-            var components = fileText.SplitIntoLines()
-                .Select(ParseComponent)
-                .SelectMany(x => new[] { new { End = x.Ends[0], Component = x }, new { End = x.Ends[1], Component = x } })
-                .ToLookup(x => x.End)
-                .ToDictionary(x => x.Key, x => x.Select(y => y.Component).ToList());
-
-            var answer = FindLongestBridge(components);
-            Output.Answer(answer);
-        }
-
         private int FindLongestBridge(Dictionary<int, List<BridgeComponent>> components)
         {
             var usedSet = new HashSet<BridgeComponent>();
             return FindLongestBridgeRecursive(0, components, usedSet).Strength;
         }
 
-        private LongestBridgeResult FindLongestBridgeRecursive(int currentSize, Dictionary<int, List<BridgeComponent>> components, HashSet<BridgeComponent> usedSet)
+        private LongestBridgeResult FindLongestBridgeRecursive(int portTypeToConnect, Dictionary<int, List<BridgeComponent>> components, HashSet<BridgeComponent> usedSet)
         {
-            if (!components.ContainsKey(currentSize))
-                return new LongestBridgeResult();
-
-            var options = components[currentSize];
             var maxScore = new LongestBridgeResult();
-            foreach (var comp in options)
+            foreach (var comp in components[portTypeToConnect].Except(usedSet))
             {
-                if (usedSet.Contains(comp))
-                    continue;
-
                 usedSet.Add(comp);
-                var otherEnd = comp.Ends[0] == currentSize ? comp.Ends[1] : comp.Ends[0];
-                var thisComponentScore = comp.Ends.Sum();
+                var otherEnd = comp.Ports[0] == portTypeToConnect ? comp.Ports[1] : comp.Ports[0];
+                var thisComponentScore = comp.Ports.Sum();
                 var nestedBest = FindLongestBridgeRecursive(otherEnd, components, usedSet);
                 nestedBest.Strength += thisComponentScore;
                 nestedBest.Length += 1;
@@ -111,24 +101,23 @@ namespace AdventOfCode2017.Solvers
             return maxScore;
         }
 
+        private BridgeComponent ParseComponent(string arg)
+        {
+            return new BridgeComponent
+            {
+                Ports = Array.ConvertAll(arg.Split('/'), int.Parse)
+            };
+        }
+
         private class LongestBridgeResult
         {
             public int Length { get; set; }
             public int Strength { get; set; }
         }
 
-        private BridgeComponent ParseComponent(string arg)
-        {
-
-            return new BridgeComponent
-            {
-                Ends = Array.ConvertAll(arg.Split('/'), int.Parse)
-            };
-        }
-
         private class BridgeComponent
         {
-            public int[] Ends { get; set; }
+            public int[] Ports { get; set; }
         }
     }
 }
